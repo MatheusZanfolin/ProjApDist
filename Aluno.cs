93095a1c851e668953af1e25e7f0fa56dd63e7cd
rafaelsiqueira100 <rafaelsiqueira100@gmail.com>
2018-04-06 10:19:46 -0300

    Adicionado métodos de conversão

---
+++ Aluno.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServidorProjeto.Models
+{
+    public class Aluno
+    {
+        public const int SEM_PROJETO = 0;
+
+        public Aluno(string ra, string nome, string email, int codProjeto)
+        {
+            if (string.IsNullOrEmpty(ra))
+                throw new Exception("Aluno: RA fornecido é nulo.");
+
+            if (string.IsNullOrEmpty(nome))
+                throw new Exception("Aluno: Nome fornecido é nulo.");
+
+            if (string.IsNullOrEmpty(email))
+                throw new Exception("Aluno: E-mail fornecido é nulo.");
+            
+            if (codProjeto < 0)
+                throw new ArgumentException("Aluno: Código de projeto inválido");
+
+            RA         = ra;
+            Nome       = nome;
+            Email      = email;
+            CodProjeto = codProjeto;
+        }
+
+        public Aluno(string ra, string nome, string email)
+        {
+            if (string.IsNullOrEmpty(ra))
+                throw new Exception("Aluno: RA fornecido é nulo.");
+
+            if (string.IsNullOrEmpty(nome))
+                throw new Exception("Aluno: Nome fornecido é nulo.");
+
+            if (string.IsNullOrEmpty(email))
+                throw new Exception("Aluno: E-mail fornecido é nulo.");
+
+            RA = ra;
+            Nome = nome;
+            Email = email;
+            CodProjeto = SEM_PROJETO;
+        }
+        public Aluno(string ra, string nome)
+        {
+            if (string.IsNullOrEmpty(ra))
+                throw new Exception("Aluno: RA fornecido é nulo.");
+
+            if (string.IsNullOrEmpty(nome))
+                throw new Exception("Aluno: Nome fornecido é nulo.");
+            RA = ra;
+            Nome = Nome;
+            CodProjeto = SEM_PROJETO;
+        }
+        public string ToString()
+        {
+            return Nome + ' '+ RA;
+        }
+        public static Aluno Converter(string aluno)
+        {
+            string nome = "";
+            string ra = "";
+            bool addandoNome = true;
+            for (int i = 0; i < aluno.Length; i++)
+            {
+                if (aluno[i] == ' ')
+                {
+                    addandoNome = false;
+                }
+                else
+                {
+                    if (addandoNome)
+                    {
+                        nome += aluno[i];
+                    }
+                    else
+                    {
+                        ra += aluno[i]
+                    }
+
+                }
+
+            }
+            return new Aluno(ra, nome);
+        }
+        public string RA { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public Int32 CodProjeto { get; private set; }
+    }
+}
---
+++ Projeto.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServidorProjeto.Models
+{
+    public class Projeto
+    {
+        public const int PROJETO_PARA_INSERIR = 0;
+
+        public Projeto(int codProjeto, string nome, string descricao, int ano)
+        {
+            if (codProjeto < 1)
+                throw new ArgumentException("Projeto: Código menor que 1");
+
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentNullException("Projeto: nome não foi fornecido");
+
+            if (string.IsNullOrEmpty(descricao))
+                throw new ArgumentNullException("Projeto: descricao não foi fornecida");
+
+            if (ano < 1)
+                throw new ArgumentException("Projeto: ano inválido");
+
+            CodProjeto = codProjeto;
+            Nome       = nome;
+            Descricao  = descricao;
+            Ano        = ano;
+        }
+        public Projeto(int codProjeto, string nome, string descricao, int ano, string alunos, string professores)
+        {
+            if (codProjeto < 1)
+                throw new ArgumentException("Projeto: Código menor que 1");
+
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentNullException("Projeto: nome não foi fornecido");
+
+            if (string.IsNullOrEmpty(descricao))
+                throw new ArgumentNullException("Projeto: descricao não foi fornecida");
+
+            if (ano < 1)
+                throw new ArgumentException("Projeto: ano inválido");
+
+            SetAlunos(alunos);
+            SetProfessores(professores);
+            CodProjeto = codProjeto;
+            nome = Nome;
+            Descricao = descricao;
+            Ano = ano;
+        }
+
+        public Projeto(string nome, string descricao, int ano)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentNullException("Projeto: nome não foi fornecido");
+
+            if (string.IsNullOrEmpty(descricao))
+                throw new ArgumentNullException("Projeto: descricao não foi fornecida");
+
+            if (ano < 1)
+                throw new ArgumentException("Projeto: ano inválido");
+
+            CodProjeto = PROJETO_PARA_INSERIR;
+            nome = Nome;
+            Descricao = descricao;
+            Ano = ano;
+        }
+        public void SetAlunos(string alunos)
+        {
+            int qtdAlunos = 0;
+            List<string> listaAlunos = new List<string>();
+            if (listaAlunos.Count > 0) {
+                string atual = listaAlunos[0].ToString();
+                int i;
+                for (i = 1; i < alunos.Length; i++)
+                {
+                    if (alunos[i] == ',')
+                    {
+                        listaAlunos.Add(atual);
+                        atual = "";
+                        qtdAlunos++;
+                        continue;
+                    }
+                    if (alunos[i-1] == ',')
+                    {
+
+                        atual = alunos[i].ToString();
+                        continue;
+                    }
+                    atual += alunos[i];
+                }
+                if (alunos[i - 1] != ',')
+                {
+                    listaAlunos.Add(atual);
+                    atual = "";
+                    qtdAlunos++;
+                }
+                this.Alunos = new Aluno[qtdAlunos];
+                
+                for(int j = 0; j < qtdAlunos; j++)
+                {
+
+                    this.Alunos[j] = Aluno.Converter(listaAlunos[j]);
+                }
+            }
+        }
+        public void SetProfessores(string professores)
+        {
+            int qtdProfessores = 0;
+            List<string> listaProfessores = new List<string>();
+            if (listaProfessores.Count > 0)
+            {
+                string atual = listaProfessores[0].ToString();
+                int i;
+                for (i = 1; i < Professores.Length; i++)
+                {
+                    if (professores[i] == ',')
+                    {
+                        listaProfessores.Add(atual);
+                        atual = "";
+                        qtdProfessores++;
+                        continue;
+                    }
+                    if (professores[i - 1] == ',')
+                    {
+                        atual = Professores[i].ToString();
+                        continue;
+                    }
+                    atual += Professores[i];
+                }
+                if (professores[i - 1] != ',')
+                {
+                    listaProfessores.Add(atual);
+                    atual = "";
+                    qtdProfessores++;
+                }
+                this.Professores = new string[qtdProfessores];
+                for (int j = 0; j < qtdProfessores; j++)
+                {
+                    this.Professores[j] = listaProfessores[j];
+                }
+            }
+        }
+        public bool ContemAluno(string aluno)
+        {
+            for(int i=0;i< Alunos.Length; i++)
+            {
+                if (Alunos[i] != aluno)
+                    return false;
+            }
+            return true;
+
+        }
+        public bool ContemProfessor(string professor)
+        {
+            for (int i = 0; i < Professores.Length; i++)
+            {
+                if (Professores[i] != professor)
+                    return false;
+            }
+            return true;
+        }
+        public int CodProjeto       { get; private set; }
+        public string Nome          { get; private set; }
+        public string Descricao     { get; private set; }
+        public int Ano              { get; private set; }
+        public Aluno[] Alunos      { get; private set; }
+        public Professor[] Professores { get; private set; }
+    }
+}
